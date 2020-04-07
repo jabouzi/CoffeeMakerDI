2fbbcdb82444322a64f62b3f1ac050a341472b2e
Skander Jabouzi <skander_jabouzi@manulife.com>
2020-04-07 12:55:17 -0400

    coffee app DI

--- app/src/main/java/com/skanderjabouzi/coffeemaker/MainActivity.kt
+++ app/src/main/java/com/skanderjabouzi/coffeemaker/MainActivity.kt
@@ -4,19 +4,13 @@ import CoffeeMaker
 import DaggerCoffeeMakerComponent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
-
-//    @Inject
-//    lateinit var coffeeMaker: CoffeeMaker
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        val coffeeMakerComponent = DaggerCoffeeMakerComponent.create()
-//        coffeeMakerComponent.inject2Activity(this)
-////        coffeeMakerComponent.brew()
-//        coffeeMaker.brew()
+        val coffeeMakerComponent = DaggerCoffeeMakerComponent.create()
+        coffeeMakerComponent.getCoffeMaker().brew()
     }
 }
